dfc154b5c2eaaad95c25b4d0b91315801e51435e
Bill Lucy <wtlucy@gmail.com>
2013-11-04 19:25:17 -0500

    Change LaTex font size
    + Change math font size, for a more clear presentation

---
+++ Source_Code/Basic_Nav_Prototype/js/jax.js
@@ -0,0 +1,8 @@
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+});
+MathJax.Hub.Config({
+"HTML-CSS": {
+scale: 85       // don't reduce font size to match surrounding font
+}
+});	
