84bb87d9864895c4064a37bd74159f8ccbae81ea
EF314159 <ef314159@vt.edu>
2013-12-01 20:18:03 -0500

    Moved sidebar into included php file
    code code code

--- Source_Code/Basic_Nav_Prototype/team.php
+++ Source_Code/Basic_Nav_Prototype/team.php
@@ -93,40 +93,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']) {
 	
 		<div class="jumbotron">
 			<div id="sidr" class="hidden-print">
-				<ul>
-					<li>
-					<a class="accordion-toggle in" data-toggle="collapse" data-target="#u1"><Strong>Unit 1 Magic Carpet Ride  <b class="caret"></b></strong></a>
-					   <p class="collapse" id="u1">
-							<a href="u1.php" title="Title"><strong> - Unit 1 Summary</strong></a>
-							<a href="u1t1.php" title="Title">- Task 1: The Carpet Ride Problem</a>
-							<a href="u1t2.php" title="Title"> - Task 2: Hide and Seek</a>
-							<a href="u1t1.php" title="Title"> - Task 3: Getting Back Home</a>
-							<a href="u1t1.php" title="Title"> - Task 4: Linear (In)dependence</a>
-					   </p>
-					</li>		
-					<li>
-					<a class="accordion-toggle" data-toggle="collapse" data-target="#u2">Unit 2  <b class="caret"></b></a>
-					   <p class="collapse" id="u2">
-							<a href="#" title="Title"> - Task 1</a>
-							<a href="#" title="Title"> - Task 2</a>
-					   </p>
-					</li>	
-					<li>				
-					<a class="accordion-toggle" data-toggle="collapse" data-target="#u3">Unit 3  <b class="caret"></b></a>
-					   <p class="collapse" id="u3">
-							<a href="#" title="Title"> - Task 1</a>
-							<a href="#" title="Title"> - Task 2</a>
-					   </p>
-					</li>
-
-					<li>
-					<a class="accordion-toggle" data-toggle="collapse" data-target="#u4">Unit 4  <b class="caret"></b></a>
-					   <p class="collapse" id="u4">
-							<a href="#" title="Title"> - Task 1</a>
-							<a href="#" title="Title"> - Task 2</a>
-					   </p>
-					</li>
-				</ul>
+				<?php include 'sidebar.php' ?>
 			</div>
 			<h1 class="text-center">Meet the IOLA Team!</h1>
 		</div>
--- Source_Code/Basic_Nav_Prototype/u1t2.php
+++ Source_Code/Basic_Nav_Prototype/u1t2.php
@@ -95,43 +95,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']) {
 
       <div class="jumbotron">
 		<div id="sidr">
-			<ul>
-				<li>
-				<a class="accordion-toggle in" data-toggle="collapse" data-target="#u1"><strong>Unit 1 Magic Carpet Ride  <b class="caret"></b></strong></a>
-				   <p class="collapse" id="u1">
-				   		<a href="u1.php" title="Title"> - Unit 1 Summary</a>
-						<a href="u1t1.php" title="Title"> - Task 1: The Carpet Ride Problem</a>
-						<a href="u1t2.php" title="Title"><strong> - Task 2: Hide and Seek</strong></a>
-						<a href="u1t3.php" title="Title"> - Task 3: Getting Back Home</a>
-						<a href="u1t4.php" title="Title"> - Task 4: Linear (In)dependence</a>
-				   </p>
-                </li>		
-				<li>
-				<a class="accordion-toggle" data-toggle="collapse" data-target="#u2">Unit 2 Italicizing N <b class="caret"></b></a>
-				   <p class="collapse" id="u2">
-						<a href="u2.php" title="Title"><strong> - Unit 2 Summary</strong></a>
-						<a href="u2t1.php" title="Title"> - Task 1: Italicizing N</a>
-						<a href="u2t2.php" title="Title"> - Task 2: Beyond the N</a>
-						<a href="u2t3.php" title="Title"> - Task 3: Pat and Jamie</a>
-						<a href="u2t4.php" title="Title"> - Task 4: Getting Back to N</a>
-				   </p>
-                </li>	
-				<li>				
-				<a class="accordion-toggle" data-toggle="collapse" data-target="#u3">Unit 3  <b class="caret"></b></a>
-				   <p class="collapse" id="u3">
-						<a href="#" title="Title"> - Task 1</a>
-						<a href="#" title="Title"> - Task 2</a>
-				   </p>
-                </li>
-
-                <li>
-				<a class="accordion-toggle" data-toggle="collapse" data-target="#u4">Unit 4  <b class="caret"></b></a>
-				   <p class="collapse" id="u4">
-						<a href="#" title="Title"> - Task 1</a>
-						<a href="#" title="Title"> - Task 2</a>
-				   </p>
-                </li>
-			</ul>
+			<?php include 'sidebar.php' ?>
 		</div>
 		
         <h1 class="text-center">Unit 1: Magic Carpet Ride</h1>
