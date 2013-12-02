cb2981a36edaf745aa10df4ffee744cad1ae3962
EF314159 <ef314159@vt.edu>
2013-12-01 23:40:39 -0500

    re-fixed buttons and sidebar.php on u2 and u2t1

--- Source_Code/Basic_Nav_Prototype/u2.php
+++ Source_Code/Basic_Nav_Prototype/u2.php
@@ -52,43 +52,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']) {
 	
 		<div class="jumbotron">
 			<div id="sidr" class="hidden-print">
-				<ul>
-					<li>
-					<a class="accordion-toggle in" data-toggle="collapse" data-target="#u1"><Strong>Unit 1 Magic Carpet Ride  <b class="caret"></b></strong></a>
-					   <p class="collapse" id="u1">
-							<a href="u1.php" title="Title"><strong> - Unit 1 Summary</strong></a>
-							<a href="u1t1.php" title="Title">- Task 1: The Carpet Ride Problem</a>
-							<a href="u1t2.php" title="Title"> - Task 2: Hide and Seek</a>
-							<a href="u1t3.php" title="Title"> - Task 3: Getting Back Home</a>
-							<a href="u1t4.php" title="Title"> - Task 4: Linear (In)dependence</a>
-					   </p>
-					</li>		
-					<li>
-					<a class="accordion-toggle" data-toggle="collapse" data-target="#u2">Unit 2  <b class="caret"></b></a>
-					   <p class="collapse" id="u2">
-							<a href="u2.php" title="Title">- Unit 2 Summary</a>
-							<a href="u2t1.php" title="Title"> - Task 1: Italicizing N</a>
-							<a href="u2t2.php" title="Title"> - Task 2: Beyond the N</a>
-							<a href="u2t3.php" title="Title"> - Task 3: Pat and Jamie </a>
-							<a href="u2t4.php" title="Title"> - Task 4: Getting Back to N</a>
-					   </p>
-					</li>	
-					<li>				
-					<a class="accordion-toggle" data-toggle="collapse" data-target="#u3">Unit 3  <b class="caret"></b></a>
-					   <p class="collapse" id="u3">
-							<a href="#" title="Title"> - Task 1</a>
-							<a href="#" title="Title"> - Task 2</a>
-					   </p>
-					</li>
-
-					<li>
-					<a class="accordion-toggle" data-toggle="collapse" data-target="#u4">Unit 4  <b class="caret"></b></a>
-					   <p class="collapse" id="u4">
-							<a href="#" title="Title"> - Task 1</a>
-							<a href="#" title="Title"> - Task 2</a>
-					   </p>
-					</li>
-				</ul>
+				<?php include 'sidebar.php' ?>
 			</div>
 			<h1 class="text-center" style="font-size:53px">Unit 2: Italicizing N Task Sequence</h1>
 			<h4>
@@ -125,7 +89,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']) {
 			<div class="accordion " id="summary">		  
 				<h2><a href="u2t1.php">Task 1: Italicizing N</a></h2>
 				<ul>
-					<a class="btn" id ="moreInfo"data-toggle="collapse" data-parent="#parent" data-target="#t1">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
+					<a class="btn btn-gray" id ="moreInfo"data-toggle="collapse" data-parent="#parent" data-target="#t1">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
 					<div class="accordion-body collapse" id = "t1">
 						<ul>
 							<blockquote>
@@ -152,7 +116,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']) {
 		
 				<h2><a href="u2t2.php">Task 2: Beyond N</a></h2>
 				<ul>
-					<a class="btn" id="moreInfo" data-toggle="collapse" data-target="#t2">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
+					<a class="btn btn-gray" id="moreInfo" data-toggle="collapse" data-target="#t2">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
 					<div class="accordion-body collapse" id = "t2">
 						<ul>
 							<blockquote>
@@ -178,7 +142,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']) {
 
 				<h2><a href="u2t3.php">Task 3: Pat and Jamie</a></h2>
 				<ul>
-					<a class="btn" id="moreInfo" data-toggle="collapse" data-target="#t3">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
+					<a class="btn btn-gray" id="moreInfo" data-toggle="collapse" data-target="#t3">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
 					<div class="accordion-body collapse" id = "t3">
 						<ul>
 							<blockquote>
@@ -204,7 +168,7 @@ if (!isset($_SESSION['user']) || !$_SESSION['user']) {
 		
 				<h2><a href="u2t4.php">Task 4: Getting Back To N</a></h2>
 				<ul>
-					<a class=" btn  hidden-print" id="moreInfo" data-toggle="collapse" data-target="#t4">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
+					<a class=" btn btn-gray" id="moreInfo" data-toggle="collapse" data-target="#t4">More Information <span class="glyphicon glyphicon-chevron-down"></span></a>
 					<div class="accordion-body collapse" id = "t4">
 						<ul>
 							<blockquote>
