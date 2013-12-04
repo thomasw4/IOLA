d83d7e2154e2464786cb36fa1279f1228bfcf48b
Bill Lucy <wtlucy@gmail.com>
2013-12-04 11:13:17 -0500

    Sidebar Updated
    +Doesn't look as terrible now

--- Source_Code/Basic_Nav_Prototype/sidebar.php
+++ Source_Code/Basic_Nav_Prototype/sidebar.php
@@ -1,38 +1,28 @@
 <ul>
-<li>
-<a class="accordion-toggle in" data-toggle="collapse" data-target="#u1"><Strong><p>Unit 1 Linear Independence<br> and Span  <b class="caret"></b></strong></p></a>
-   <p class="collapse" id="u1">
-		<a href="u1.php" title="Title"><strong> - Unit 1 Summary</strong></a>
-		<a href="u1t1.php" title="Title"> - Task 1: The Carpet Ride Problem</a>
-		<a href="u1t2.php" title="Title"> - Task 2: Hide and Seek</a>
-		<a href="u1t3.php" title="Title"> - Task 3: Getting Back Home</a>
-		<a href="u1t4.php" title="Title"> - Task 4: Linear (In)dependence</a>
-   </p>
-</li>		
-<li>
-<a class="accordion-toggle" data-toggle="collapse" data-target="#u2"><p>Unit 2 Matrices as Transformations <b class="caret"></b></p></a>
-   <p class="collapse" id="u2">
-		<a href="u2.php" title="Title"><strong> - Unit 2 Summary</strong></a>
-		<a href="u2t0.php" title="Title"> - Introductory Lesson</a>
-		<a href="u2t1.php" title="Title"> - Task 1: Italicizing N</a>
-		<a href="u2t2.php" title="Title"> - Task 2: Beyond the N</a>
-		<a href="u2t3.php" title="Title"> - Task 3: Pat and Jamie</a>
-		<a href="u2t4.php" title="Title"> - Task 4: Getting Back to N</a>
-   </p>
-</li>	
-<li>				
-<a class="accordion-toggle" data-toggle="collapse" data-target="#u3">Unit 3  <b class="caret"></b></a>
-   <p class="collapse" id="u3">
-		<a href="#" title="Title"> - Task 1</a>
-		<a href="#" title="Title"> - Task 2</a>
-   </p>
-</li>
-
-<li>
-<a class="accordion-toggle" data-toggle="collapse" data-target="#u4">Unit 4  <b class="caret"></b></a>
-   <p class="collapse" id="u4">
-		<a href="#" title="Title"> - Task 1</a>
-		<a href="#" title="Title"> - Task 2</a>
-   </p>
-</li>
+	<li>
+		<a class="accordion-toggle in" data-toggle="collapse" data-target="#u1">Unit 1: Linear Independence<br> and Span  <b class="caret"></b></a>
+		   <div class="collapse" id="u1">
+		   <ul>
+				<li><a href="u1.php" title="Title"> - Unit 1 Summary</a></li>
+				<li><a href="u1t1.php" title="Title"> - Task 1: The Carpet Ride Problem</a></li>
+				<li><a href="u1t2.php" title="Title"> - Task 2: Hide and Seek</a></li>
+				<li><a href="u1t3.php" title="Title"> - Task 3: Getting Back Home</a></li>
+				<li><a href="u1t4.php" title="Title"> - Task 4: Linear (In)dependence</a></li>
+			</ul>
+		    </div>
+	</li>	
+	<br>	
+	<li>
+		<a class="accordion-toggle" data-toggle="collapse" data-target="#u2">Unit 2: Matrices as Transformations <b class="caret"></b></a>
+			<div class="collapse" id="u2">
+		   	<ul>
+				<li><a href="u2.php" title="Title"> - Unit 2 Summary</a></li>
+				<li><a href="u2t0.php" title="Title"> - Introductory Lesson</a></li>
+				<li><a href="u2t1.php" title="Title"> - Task 1: Italicizing N</a></li>
+				<li><a href="u2t2.php" title="Title"> - Task 2: Beyond the N</a></li>
+				<li><a href="u2t3.php" title="Title"> - Task 3: Pat and Jamie</a></li>
+				<li><a href="u2t4.php" title="Title"> - Task 4: Getting Back to N</a></li>
+			</ul>
+			</div>
+	</li>	
 </ul>
